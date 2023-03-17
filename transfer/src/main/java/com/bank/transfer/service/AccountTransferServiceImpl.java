2db5dedd3744c77cea6899323713b3efa4ab0512
Dmitry Tebryaev <tebr5923@mail.ru>
2023-03-17 16:30:44 +0700

    add log into AccountTransferServiceImpl

--- transfer/src/main/java/com/bank/transfer/service/AccountTransferServiceImpl.java
+++ transfer/src/main/java/com/bank/transfer/service/AccountTransferServiceImpl.java
@@ -2,6 +2,7 @@ package com.bank.transfer.service;
 
 import com.bank.transfer.entity.AccountTransfer;
 import com.bank.transfer.repository.AccountTransferRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,9 +10,10 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
-public class AccountTransferServiceImpl implements AccountTransferService{
+public class AccountTransferServiceImpl implements AccountTransferService {
 
     private final AccountTransferRepository accountTransferRepository;
 
@@ -23,34 +25,53 @@ public class AccountTransferServiceImpl implements AccountTransferService{
     @Override
     @Transactional
     public void save(AccountTransfer accountTransfer) {
+        log.trace("try to save accountTransfer: {}", accountTransfer);
         accountTransferRepository.save(accountTransfer);
+        log.trace("save accountTransfer success, id={}", accountTransfer.getId());
     }
 
     @Override
     @Transactional
     public void update(Long id, AccountTransfer accountTransfer) {
         accountTransfer.setId(id);
+
+        log.trace("try to update accountTransfer: {}", accountTransfer);
         accountTransferRepository.save(accountTransfer);
+        log.trace("update accountTransfer success, id={}", accountTransfer.getId());
     }
 
     @Override
     @Transactional
     public void deleteById(Long id) {
+        log.trace("try to delete accountTransfer with id={}", id);
         accountTransferRepository.deleteById(id);
+        log.trace("success delete accountTransfer with id={}", id);
     }
 
     @Override
     public Optional<AccountTransfer> getById(Long id) {
-        return accountTransferRepository.findById(id);
+        log.trace("try to get accountTransfer by id={}", id);
+        var accountTransfer = accountTransferRepository.findById(id);
+        log.trace("getById accountTransfer success, accountTransfer.isPresent() = {}", accountTransfer.isPresent());
+
+        return accountTransfer;
     }
 
     @Override
     public Optional<AccountTransfer> getByAccountNumber(Long accountNumber) {
-        return accountTransferRepository.getByAccountNumber(accountNumber);
+        log.trace("try to get accountTransfer by accountNumber={}", accountNumber);
+        var accountTransfer = accountTransferRepository.getByAccountNumber(accountNumber);
+        log.trace("getByAccountNumber accountTransfer success, accountTransfer.isPresent() = {}", accountTransfer.isPresent());
+
+        return accountTransfer;
     }
 
     @Override
     public List<AccountTransfer> getAll() {
-        return accountTransferRepository.findAll();
+        log.trace("try to get All accountTransfers");
+        var accountTransfers =accountTransferRepository.findAll();
+        log.trace("getAll accountTransfers success, count = {}", accountTransfers.size());
+
+        return accountTransfers;
     }
 }
