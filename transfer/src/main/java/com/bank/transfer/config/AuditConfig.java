59e8c752a9874511417d79ed3982368774aa5d70
Dmitry Tebryaev <tebr5923@mail.ru>
2023-03-20 23:07:38 +0700

    begin audit

---
+++ transfer/src/main/java/com/bank/transfer/config/AuditConfig.java
@@ -0,0 +1,10 @@
+package com.bank.transfer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+
+}
